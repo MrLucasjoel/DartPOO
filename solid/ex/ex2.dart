7708ff4013d15c7694833f5b0ddcea5a65e8016d
lucas joel <lucas.joel1628@gmail.com>
2025-05-08 07:54:37 -0300

    ex

--- solid/ex/ex2.dart
+++ solid/ex/ex2.dart
@@ -1,26 +1,21 @@
 
-abstract class CalculadoraSalario {
+class CalculadoraSalario {
 double calcular(String salario);
-}
-
-class Gerente implements CalculadoraSalario{
-  @override
-  double calcular(String calcular){
-    return 100 * 0.2;
-  }
 }
 
 class Gerente implements CalculadoraSalario {
   @override
   double calcular(String calcular) => 5000 - 1500;
+  
 }
 
 class Desenvolvedor implements CalculadoraSalario {
   @override
   double calcular(String calcular) => 4000 - 1500;
 }
+
  void main(List<String> args) {
-   final salario = calcular();
+   CalculadoraSalario salario = CalculadoraSalario();
   print("Imposto eletronicos: ${salario.calcular(Gerente())}");
   print("Imposto alimentos: ${salario.calcular(Desenvolvedor())}");
  }
